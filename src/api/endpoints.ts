d1b72f4c5a95a581b46126949778366a29d5d9fd
yeahzee0421 <ydw08250421@gmail.com>
2024-07-30 21:49:45 +0900

    feat: BASE_URL 수정 및 API 엔드포인트 추가

---
+++ src/api/endpoints.ts
@@ -0,0 +1,8 @@
+export const BASE_ENDPOINTS = {
+  MEMBER: '/api/members',
+  CATEGORY: '/api/categories',
+  ORDER: '/api/order',
+  PRODUCTS: '/api/products',
+  KAKAO_OAUTH: '/kakao',
+  WISH: '/api/wishes',
+};
--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -16,8 +16,9 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   return instance;
 };
 
-export const BASE_URL = 'https://api.example.com';
-// TODO: 추후 서버 API 주소 변경 필요
+export const BASE_URL_CHOI = 'http://13.125.199.167:8080';
+export const BASE_URL_KO = 'http://3.36.54.48:8080';
+
 export const fetchInstance = initInstance({
   baseURL: 'https://api.example.com',
 });
