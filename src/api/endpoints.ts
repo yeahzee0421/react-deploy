c189453992ecde9099525629ff9ad79e70b16d51
yeahzee0421 <ydw08250421@gmail.com>
2024-07-30 21:57:18 +0900

    docs: step1 리드미 작성

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,7 +1,7 @@
 export const BASE_ENDPOINTS = {
   MEMBER: '/api/members',
   CATEGORY: '/api/categories',
-  ORDER: '/api/order',
+  ORDER: '/api/orders',
   PRODUCTS: '/api/products',
   KAKAO_OAUTH: '/kakao',
   WISH: '/api/wishes',
--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -16,6 +16,7 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   return instance;
 };
 
+export const BASE_URL = 'https://api.example.com';
 export const BASE_URL_CHOI = 'http://13.125.199.167:8080';
 export const BASE_URL_KO = 'http://3.36.54.48:8080';
 
